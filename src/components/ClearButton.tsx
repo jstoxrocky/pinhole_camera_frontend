45f9bec12689c3fa52f47c846a14be22e1b493ce
jstoxrocky <joestox@gmail.com>
2018-09-30 20:25:07 +0200

    fix2

---
+++ src/components/ClearButton.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import * as Redux from 'redux';
+import { connect } from 'react-redux';
+import { Button } from '@material-ui/core';
+// import { clearCamera } from '../store/camera/actions';
+
+interface DispatchProps {
+  dispatch: Redux.Dispatch<any>;
+}
+type Props = DispatchProps;
+
+class ClearButton extends React.Component<Props> {
+
+  constructor(props: Props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    // this.props.dispatch((clearCamera()));
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Button variant="contained" color="primary" onClick={this.handleClick}>CLEAR</Button>
+      </React.Fragment>
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch: Redux.Dispatch<any>) {
+  return { dispatch };
+}
+
+export default connect<DispatchProps>(mapDispatchToProps)(ClearButton);
